60d0d95bc171aa01090901d78e3dbf05e0af8f5d
HanTrung <hantrungcui@gmail.com>
2024-11-10 16:17:19 +0700

    update slide show home

--- js/home.js
+++ js/home.js
@@ -1,19 +1,42 @@
-let slideIndex = 1;
-showSlides();
+let slideIndex = 0;
+        const slides = document.getElementsByClassName("mySlides");
+        const dots = document.getElementsByClassName("dot");
+        const totalSlides = slides.length;
 
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
+        function showSlides() {
+            const offset = -slideIndex * 100;
+            document.querySelector('.w3-content').style.transform = `translateX(${offset}%)`;
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
 
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
+            for (let i = 0; i < totalSlides; i++) {
+                dots[i].className = dots[i].className.replace(" active", ""); // Xóa lớp active
+            }
+            dots[slideIndex].className += " active";
+        }
 
-  slides[slideIndex - 1].style.display = "block";
+        function plusDivs(n) {
+            slideIndex += n;
 
-  setTimeout(showSlides, 3000);
-}
+            if (slideIndex < 0) {
+                slideIndex = totalSlides - 1;
+            } else if (slideIndex >= totalSlides) {
+                slideIndex = 0;
+            }
+
+            showSlides();
+        }
+
+        function currentSlide(n) {
+            slideIndex = n;
+            showSlides();
+        }
+
+        setInterval(() => {
+            slideIndex++;
+            if (slideIndex >= totalSlides) {
+                slideIndex = 0;
+            }
+            showSlides();
+        }, 3000);
+
+        showSlides();
