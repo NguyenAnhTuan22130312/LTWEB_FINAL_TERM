0003ab0c90fb841e97d448aba20c69bb7ee4df12
Nguyen Anh Tuan <22130311@st.hcmuaf.edu.vn>
2024-11-12 22:07:33 +0700

    Update signin

--- js/signin.js
+++ js/signin.js
@@ -1,32 +1,11 @@
-document.querySelectorAll("input").forEach((item) => {
-  item.addEventListener("focus", function () {
-    item.previousElementSibling.className = "label-selected";
-  });
-  item.addEventListener("blur", function () {
-    console.log(item.value);
-    if (item.value === "") {
-      item.previousElementSibling.className = "";
-    }
-  });
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-document.getElementById("registerLink").addEventListener("click", function () {
-  if (window.innerWidth < 600) {
-    document.getElementById("signUp").style.display = "block";
-    document.getElementById("login").style.display = "none";
-  } else {
-    document.getElementById("overlay").style.transform =
-      "translate(550px , -25px)";
-  }
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
-document.getElementById("loginLink").addEventListener("click", function () {
-  if (window.innerWidth < 600) {
-    document.getElementById("login").style.display = "block";
-    document.getElementById("signUp").style.display = "none";
-  } else {
-    document.getElementById("overlay").style.transform =
-      "translate(0px , -25px)";
-  }
-});
-
-
