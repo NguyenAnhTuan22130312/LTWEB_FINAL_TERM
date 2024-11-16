c5af6de4ee946493d933abd49d59bce09e120a77
Nguyen Anh Tuan <22130311@st.hcmuaf.edu.vn>
2024-11-16 23:25:04 +0700

    Update interface checkout

--- js/check-out.js
+++ js/check-out.js
@@ -29,3 +29,65 @@ for (let i = 0; i < pickdate.length; i++) {
     this.classList.add("active");
   };
 }
+
+// Xu ly hinh thuc giao hang
+let giaotannoi = document.querySelector("#giaotannoi");
+let tudenlay = document.querySelector("#tudenlay");
+let tudenlayGroup = document.querySelector("#tudenlay-group");
+let chkShip = document.querySelectorAll(".chk-ship");
+
+tudenlay.addEventListener("click", () => {
+  giaotannoi.classList.remove("active");
+  tudenlay.classList.add("active");
+  chkShip.forEach((item) => {
+    item.style.display = "none";
+  });
+  tudenlayGroup.style.display = "block";
+});
+
+giaotannoi.addEventListener("click", () => {
+  tudenlay.classList.remove("active");
+  giaotannoi.classList.add("active");
+  tudenlayGroup.style.display = "none";
+  chkShip.forEach((item) => {
+    item.style.display = "flex";
+  });
+});
+
+//Xu ly phuong thuc thanh toan
+let tienmat = document.querySelector("#tienmat");
+let thenganhang = document.querySelector("#thenganhang");
+let vidientu = document.querySelector("#vidientu");
+let thenganhangGroup = document.querySelector("#thenganhang-group");
+let vidientuGroup = document.querySelector("#vidientu-group");
+let chkPay = document.querySelectorAll(".chk-pay");
+
+tienmat.addEventListener("click", () => {
+  thenganhang.classList.remove("active");
+  vidientu.classList.remove("active");
+  tienmat.classList.add("active");
+  chkPay.forEach((item) => {
+    item.style.display = "none";
+  });
+});
+
+thenganhang.addEventListener("click", () => {
+  tienmat.classList.remove("active");
+  vidientu.classList.remove("active");
+  thenganhang.classList.add("active");
+  chkPay.forEach((item) => {
+    item.style.display = "none";
+  });
+  thenganhangGroup.style.display = "block";
+});
+
+vidientu.addEventListener("click", () => {
+  tienmat.classList.remove("active");
+  thenganhang.classList.remove("active");
+  vidientu.classList.add("active");
+  thenganhangGroup.style.display = "none";
+  chkPay.forEach((item) => {
+    item.style.display = "none";
+  });
+  vidientuGroup.style.display = "block";
+});
