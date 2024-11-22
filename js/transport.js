d9391f2cadeb092e3ad0901bcbc559f04bc041a7
anhtuan2908 <nguyentuan29082004@gmail.com>
2024-11-23 01:54:28 +0700

    quick up

--- js/transport.js
+++ js/transport.js
@@ -2,3 +2,6 @@ document.getElementById("btn_transport").addEventListener("click", function () {
     window.location.href = "../html/transport.html"; // Đường dẫn đến trang HTML đích
   });
   
+  document.getElementById("btn_all").addEventListener("click", function () {
+    window.location.href = "../html/PurchaseOrder.html"; // Đường dẫn đến trang HTML đích
+  });
