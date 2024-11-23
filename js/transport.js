c1675bd938fb6e62e9bf3413a50bcfc1d3a17d34
anhtuan2908 <nguyentuan29082004@gmail.com>
2024-11-23 12:58:07 +0700

    them trang van chuyen va trang huy hang

--- js/transport.js
+++ js/transport.js
@@ -5,3 +5,11 @@ document.getElementById("btn_transport").addEventListener("click", function () {
   document.getElementById("btn_all").addEventListener("click", function () {
     window.location.href = "../html/PurchaseOrder.html"; // Đường dẫn đến trang HTML đích
   });
+
+  document.getElementById("btn_delivered").addEventListener("click", function () {
+    window.location.href = "../html/Done_Deliver.html"; // Đường dẫn đến trang HTML đích
+  });
+  
+  document.getElementById("btn_cancelled").addEventListener("click", function () {
+    window.location.href = "../html/Cancelled.html"; // Đường dẫn đến trang HTML đích
+  });
